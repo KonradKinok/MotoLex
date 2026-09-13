7c4ae85f20ad34d44adf2174d54c8589234baf5b
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2026-09-13 22:12:54 +0200

    Update Advertisement.tsx

--- src/components/Advertisement/Advertisement.tsx
+++ src/components/Advertisement/Advertisement.tsx
@@ -5,12 +5,12 @@ export function Advertisement() {
     <aside className={styles.advertisingColumn} aria-label="Reklamy">
       <section className={styles.advertisement}>
         <p className={styles.advertisementLabel}>Obrazek</p>
-        <div className={styles.advertisementPlaceholder}>300 × 250</div>
+        <div className={styles.advertisementPlaceholder}>300 x 250</div>
       </section>
 
       <section className={styles.advertisement}>
         <p className={styles.advertisementLabel}>Obrazek</p>
-        <div className={styles.advertisementPlaceholder}>300 × 250</div>
+        <div className={styles.advertisementPlaceholder}>300 x 250</div>
       </section>
     </aside>
   );
