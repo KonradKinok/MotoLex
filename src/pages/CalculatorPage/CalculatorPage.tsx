cc5ab8e98c364aedf61d7132dead7efbb359d323
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2026-09-13 20:03:47 +0200

    CalculatorPage szkielet 13..09.2026

--- src/pages/CalculatorPage/CalculatorPage.tsx
+++ src/pages/CalculatorPage/CalculatorPage.tsx
@@ -1,11 +1,27 @@
+import { Link } from "react-router";
+import styles from "./CalculatorPage.module.scss";
+
 function CalculatorPage() {
   return (
-    <article>
-      <h1>Kalkulator</h1>
-      <p>
-        Treści dotyczące kalkulatorów pojazdów zostaną dodane po ich weryfikacji
-        merytorycznej.
-      </p>
+    <article className={styles.calculatorPageMainContainer}>
+      <h1>Kalkulatory</h1>
+
+      <div className={styles.calculatorLinks}>
+        <Link to="kary" className={styles.calculatorLink}>
+          <img src="" alt="" width={800} height={600} />
+          <span>Kary</span>
+        </Link>
+
+        <Link to="vin" className={styles.calculatorLink}>
+          <img src="" alt="" width={800} height={600} />
+          <span>VIN</span>
+        </Link>
+
+        <Link to="trwala-utrata" className={styles.calculatorLink}>
+          <img src="" alt="" width={800} height={600} />
+          <span>Trwała utrata</span>
+        </Link>
+      </div>
     </article>
   );
 }
