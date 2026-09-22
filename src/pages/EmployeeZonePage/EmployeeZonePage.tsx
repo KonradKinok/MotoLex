83689dda85688bbb2e59cd598bf5c32d8ca0b78c
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2026-09-22 21:05:37 +0200

    homologacja 22.09.2026

--- src/pages/EmployeeZonePage/EmployeeZonePage.tsx
+++ src/pages/EmployeeZonePage/EmployeeZonePage.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
 import { PageMetadata } from "../../components/PageMetaData/PageMetaData";
 import { ROUTES, APPLICATION_NAME } from "../../config/routes";
+import HomologacjaVertical from "../../assets/images/employeeZonePage/HomologacjaVertical.webp";
+import styles from "./EmployeeZonePage.module.scss";
 
 function EmployeeZonePage() {
   return (
@@ -9,12 +12,25 @@ function EmployeeZonePage() {
         description="Baza wiedzy dla pracowników wydziałów komunikacji. Miejsce na komunikaty instytucji, orzeczenia i specjalistyczne materiały wspierające codzienną pracę."
         path={ROUTES.employees}
       />
-      <section>
-        <h1>Baza wiedzy dla pracowników</h1>
-        <p>
-          W tym miejscu znajdą się komunikaty instytucji, orzeczenia oraz
-          specjalistyczne materiały dla pracowników wydziałów komunikacji.
-        </p>
+      <section className={styles.employeeZonePageMainContainer}>
+        <div>
+          <h1 className={styles.headerH1}>Baza wiedzy dla pracowników</h1>
+          <p>
+            W tym miejscu znajdują się komunikaty instytucji, orzeczenia oraz
+            specjalistyczne materiały dla pracowników wydziałów komunikacji.
+          </p>
+        </div>
+        <div className={styles.employeeZoneLinksContainer}>
+          <Link to="homologacja" className={styles.employeeZoneLink}>
+            <img
+              src={HomologacjaVertical}
+              alt="Homologacja"
+              width={600}
+              height={800}
+              className={styles.img}
+            />
+          </Link>
+        </div>
       </section>
     </>
   );
